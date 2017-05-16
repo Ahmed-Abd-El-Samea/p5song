c32c344dc5a88bfc7a9c0747de944f8ca203643b
Ahmed-Abdo <ahmedabdo6868@gmail.com>
2017-05-16 07:19:39 +0200

    adjusting the stroke

--- sketch.js
+++ sketch.js
@@ -44,15 +44,17 @@ function draw() {
 	var spectrum = fft.analyze();
 	var waveform = fft.waveform();
 	noFill();
+
 	beginShape();
 	stroke(12,132,68); // waveform is red
-	strokeWeight(5);
+	strokeWeight(2);
 	for (var i = 0; i< waveform.length; i++){
 	  var x = map(i, 0, waveform.length, 0, width);
 	  var y = map( waveform[i], -1, 1, 0, height);
 	  vertex(x,y);
 	}
 	endShape();
+
 	// console.log(spectrum);
 	// fill(0,255,0); // spectrum is green
 	// for (var i = 0; i< spectrum.length; i++){
